2406b4ccfb05a2ad5b738b5ee156541b4f54f308
Foysal ahammmed <foysalahammed07@gmail.com>
2022-11-10 10:20:03 +0600

    Day-six last updated

--- Day-six/script.js
+++ Day-six/script.js
@@ -40,6 +40,235 @@ const restaurant = {
   },
 };
 
+const flights =
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+//console.log(flights.split("+"));
+
+const getCode = (str) => str.slice(0, 3).toUpperCase();
+for (const flight of flights.split("+")) {
+  const [type, from, to, time] = flight.split(";");
+  //console.log(type, from, to, time);
+
+  const output = `${type.startsWith("_Delayed") ? "🔴" : " "}${type.replaceAll(
+    "_",
+    " "
+  )} ${getCode(from)} ${getCode(to)} ${time.replace(":", "h")}`.padStart(37);
+  console.log(output);
+}
+
+/*
+
+////////////////////////////////
+// CODING CHALLANGE #4
+
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
+
+document.querySelector("button").addEventListener("click", function () {
+  const text = document.querySelector("textarea").value;
+  const rows = text.split("\n");
+  console.log(rows);
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split("_");
+    //console.log(row, first, second);
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${"✅".repeat(i + 1)}`);
+  }
+});
+
+*/
+
+// const capitaliseName = function (name) {
+//   const names = name.split(" ");
+//   const namesUpper = [];
+
+//   for (const n of names) {
+//     namesUpper.push(n[0].toUpperCase() + n.slice(1));
+//   }
+//   console.log(namesUpper.join(" "));
+// };
+
+// capitaliseName("jessica ann smit davis");
+// capitaliseName("jonas schmedtmann");
+/*
+const ariLine = "TAP Air Portugal";
+
+console.log(ariLine.toLowerCase());
+console.log(ariLine.toUpperCase());
+
+const passenger = "jOnAS"; // Jonas
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+const email = "hello@jonas.io";
+const loginEmail = "  Hello@Jonas.Io \n";
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+const priceGB = "280,97£";
+const priceUS = priceGB.replace("£", "$").replace(",", ".");
+console.log(priceUS);
+
+// Practice exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes("knife") || baggage.includes("gun")) {
+    console.log("You are NOT allowed on board");
+  } else {
+    console.log("Welcome aboard!");
+  }
+};
+checkBaggage("I have a laptop, some Food and a pocket Knife");
+checkBaggage("Socks and camera");
+checkBaggage("Got some snacks and a gun for protection");
+*/
+
+// const ariLine = "TAP Air Portugal";
+// const plane = "A320";
+
+// console.log(plane[0]);
+// console.log(plane[1]);
+// console.log(plane[2]);
+
+// console.log(ariLine.length);
+// console.log("B737".length);
+
+// console.log(ariLine.indexOf("r"));
+// console.log(ariLine.lastIndexOf("r"));
+// console.log(ariLine.indexOf("portugal"));
+
+/*
+////////////////////////////////
+// CODING CHALLANGE #3
+
+const gameEvents = new Map([
+  [17, "⚽️ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽️ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽️ GOAL"],
+  [80, "⚽️ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
+
+// 1. Solution
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2. Solution
+gameEvents.delete(64);
+
+// 3. Solution
+console.log(
+  `An event happend, on average , ever ${90 / gameEvents.size} minutes`
+);
+
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(
+  `An event happend, on average , ever ${time / gameEvents.size} minutes`
+);
+
+// 4. Solution
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? "FIRST" : "SECOND";
+  console.log(`[${half} HALF] ${min} : ${event}`);
+}
+*/
+
+/*
+const question = new Map([
+  ["question", "What is the best programming language in the world?"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "JavaScript"],
+  ["correct", 3],
+  [true, "Correct 🎉"],
+  [false, "Try again!"],
+]);
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+for (const [key, values] of question) {
+  if (typeof key === "number") console.log(`Answer ${key} : ${values}`);
+}
+*/
+
+/*
+const rest = new Map();
+
+rest.set("name", "Classico Italiano");
+rest.set(1, "Firense, Italy");
+
+console.log(rest.set(2, "Lisbon, Portugal"));
+
+rest
+  .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+  .set("open", 11)
+  .set("Close", 23)
+  .set(true, "We are open :d");
+
+console.log(rest.get("name"));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+
+console.log(rest.has("categories"));
+rest.delete(2);
+console.log(rest);
+console.log(rest.size);
+*/
+
+/*
+const orderSet = new Set([
+  "Pasta",
+  "Pizza",
+  "Pizza",
+  "Risotto",
+  "Pasta",
+  "Pizza",
+]);
+
+console.log(orderSet);
+console.log(new Set("Jonas"));
+
+console.log(orderSet.size);
+
+console.log(orderSet.has("Pizza"));
+console.log(orderSet.has("Bread"));
+
+orderSet.add("Garlic Bread");
+orderSet.delete("Risotto");
+
+console.log(orderSet);
+
+for (const order of orderSet) console.log(order);
+
+const staff = ["Waiter", "Chef", "Waiter", "Chef", "Waiter", "Manager"];
+const statffUnique = new Set(staff);
+console.log(statffUnique);
+*/
+
+/*
+
 /////////////////////////////////
 // CODING CHALLANGE #2
 
@@ -104,6 +333,8 @@ for (const [team, odd] of Object.entries(game.odds)) {
 }
 
 // 4. Solution
+
+*/
 
 /*
 const properties = Object.keys(openingHours);
