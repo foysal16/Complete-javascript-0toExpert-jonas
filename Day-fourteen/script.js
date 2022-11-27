6b37b925b865145f8fab34f7d86adbc028cf0c61
Foysal ahammmed <foysalahammed07@gmail.com>
2022-11-27 13:11:59 +0600

    Day-fourteen last update

--- Day-fourteen/script.js
+++ Day-fourteen/script.js
@@ -1,42 +1,28 @@
-///////////////////////////////////////
-// Exporting and Importing in ES6 Modules
-
-// Importing module
-// import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
-// addToCart('bread', 5);
-// console.log(price, tq);
-
-console.log('Importing module');
-// console.log(shippingCost);
-
-// import * as ShoppingCart from './shoppingCart.js';
-// ShoppingCart.addToCart('bread', 5);
-// console.log(ShoppingCart.totalPrice);
-
-// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
-// console.log(price);
-
-import add, { cart } from './shoppingCart.js';
-add('pizza', 2);
-add('bread', 5);
-add('apples', 4);
-
-console.log(cart);
 /*
 
+// import { addToCart, totalPrice as price, tq } from "./shoppingCart.js";
 
-///////////////////////////////////////
-// Top-Level Await (ES2022)
+// console.log("Importing Module");
 
-// console.log('Start fetching');
-// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// addToCart("Bread", 5);
+// console.log(price, tq);
+
+console.log("Importing Module");
+
+import * as ShoppingCart from "./shoppingCart.js";
+
+ShoppingCart.addToCart("bread", 5);
+console.log(ShoppingCart.totalPrice);
+
+// const res = await fetch("https://jsonplaceholder.typicode.com/posts");
 // const data = await res.json();
+
 // console.log(data);
-// console.log('Something');
 
 const getLastPost = async function () {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   const data = await res.json();
+  console.log(data);
 
   return { title: data.at(-1).title, text: data.at(-1).body };
 };
@@ -44,16 +30,14 @@ const getLastPost = async function () {
 const lastPost = getLastPost();
 console.log(lastPost);
 
-// Not very clean
-// lastPost.then(last => console.log(last));
+//Not very clean
+//lastPost.then((last) => console.log(last));
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+*/
 
-
-///////////////////////////////////////
-// The Module Pattern
-
+/*
 const ShoppingCart2 = (function () {
   const cart = [];
   const shippingCost = 10;
@@ -63,12 +47,13 @@ const ShoppingCart2 = (function () {
   const addToCart = function (product, quantity) {
     cart.push({ product, quantity });
     console.log(
-      `${quantity} ${product} added to cart (sipping cost is ${shippingCost})`
+      `${quantity} ${product} added to cart and shipping cost ${shippingCost}`
     );
   };
 
   const orderStock = function (product, quantity) {
-    console.log(`${quantity} ${product} ordered from supplier`);
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} Order from supplier`);
   };
 
   return {
@@ -79,67 +64,29 @@ const ShoppingCart2 = (function () {
   };
 })();
 
-ShoppingCart2.addToCart('apple', 4);
-ShoppingCart2.addToCart('pizza', 2);
-console.log(ShoppingCart2);
-console.log(ShoppingCart2.shippingCost);
-
-
-///////////////////////////////////////
-// CommonJS Modules
-// Export
-export.addTocart = function (product, quantity) {
-  cart.push({ product, quantity });
-  console.log(
-    `${quantity} ${product} added to cart (sipping cost is ${shippingCost})`
-  );
-};
-
-// Import
-const { addTocart } = require('./shoppingCart.js');
+ShoppingCart2.addToCart("apple", 4);
+ShoppingCart2.addToCart("pizza", 2);
 */
 
-///////////////////////////////////////
-// Introduction to NPM
-// import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
-import cloneDeep from 'lodash-es';
+// const addToCart = function (product, quantity) {
+//   cart.push({ product, quantity });
+//   console.log(
+//     `${quantity} ${product} added to cart and shipping cost ${shippingCost}`
+//   );
+// };
 
-const state = {
-  cart: [
-    { product: 'bread', quantity: 5 },
-    { product: 'pizza', quantity: 5 },
-  ],
-  user: { loggedIn: true },
-};
-const stateClone = Object.assign({}, state);
-const stateDeepClone = cloneDeep(state);
+//import cloneDeep from "./node_modules/lodash-es/cloneDeep.js";
 
-state.user.loggedIn = false;
-console.log(stateClone);
+// const state = {
+//   cart: [
+//     { produc: "Bread", quantity: 5 },
+//     { produc: "pizza", quantity: 5 },
+//   ],
+//   user: { loggedIn: true },
+// };
 
-console.log(stateDeepClone);
+// const stateClone = Object.assign({}, state);
+// console.log(stateClone);
 
-if (module.hot) {
-  module.hot.accept();
-}
-
-class Person {
-  #greeting = 'Hey';
-  constructor(name) {
-    this.name = name;
-    console.log(`${this.#greeting}, ${this.name}`);
-  }
-}
-const jonas = new Person('Jonas');
-
-console.log('Jonas' ?? null);
-
-console.log(cart.find(el => el.quantity >= 2));
-Promise.resolve('TEST').then(x => console.log(x));
-
-import 'core-js/stable';
-// import 'core-js/stable/array/find';
-// import 'core-js/stable/promise';
-
-// Polifilling async functions
-import 'regenerator-runtime/runtime';
+// const stateDeepClone = cloneDeep(state);
+// console.log(stateDeepClone);
